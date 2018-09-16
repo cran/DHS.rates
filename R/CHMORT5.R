7632e8c6846da77d59b3c65811741810c928e78c
Mahmoud Elkasabi <mahmoud.elkasabi@icf.com>
2018-09-16 15:30:15 +0000

    version 0.4.0

--- R/CHMORT5.R
+++ R/CHMORT5.R
@@ -1,88 +1,181 @@
 # CHMORT5 function: calculate Children death probabilities
-# NBIRTHS function: calculate total births contributed to the exposure (full or partial contribution)
+# NBIRTHS function: calculate approximated number of children exposed to the risk of mortality
+# NBIRTHSW function: calculate weighted approximated number of children exposed to the risk of mortality
+# DEFT function: calculate approximated design effect
 # Mahmoud Elkasabi
 # 04/03/2018
 # Edited on 06/10/2018
+# Edited on 09/12/2018
 
 ### Child mortality rates
 CHMORT5 <- function(Data.Name, PeriodEnd = NULL) {
-  ageseg <- list(c(0, 1), c(1, 3), c(3, 6), c(6, 12), c(12, 24), c(24, 36), c(36, 48), c(48, 60))
-  names(ageseg) <- c(1, 2, 3, 4, 5, 6, 7, 8)
+      ageseg <- list(c(0, 1), c(1, 3), c(3, 6), c(6, 12), c(12, 24), c(24, 36), c(36, 48), c(48, 60))
+      names(ageseg) <- c(1, 2, 3, 4, 5, 6, 7, 8)
 
-  deathprob <- numeric(length = 8)
-  names(deathprob) <- names(ageseg)
+      deathprob <- numeric(length = 8)
+      names(deathprob) <- names(ageseg)
 
-  for (i in seq_along(ageseg)) {
-    segdata <- Data.Name[which((Data.Name$b7 >= ageseg[[i]][1]) | is.na(Data.Name$b7)), ]
+      for (i in seq_along(ageseg)) {
+        segdata <- Data.Name[which((Data.Name$b7 >= ageseg[[i]][1]) | is.na(Data.Name$b7)), ]
 
-    segdata$exposure[segdata$b3 >= (segdata$tl - ageseg[[i]][2]) &
-                       segdata$b3 < (segdata$tl - ageseg[[i]][1]) ] <- 0.5
+        segdata$exposure <- NA
 
-    segdata$exposure[segdata$b3 >= (segdata$tl - ageseg[[i]][1]) &
-                       segdata$b3 < (segdata$tu - ageseg[[i]][2]) ] <- 1
+        segdata$exposure[segdata$b3 >= (segdata$tl - ageseg[[i]][2]) &
+                           segdata$b3 < (segdata$tl - ageseg[[i]][1]) ] <- 0.5
 
-    segdata$exposure[segdata$b3 >= (segdata$tu - ageseg[[i]][2]) &
-                       segdata$b3 < (segdata$tu - ageseg[[i]][1]) ] <- 0.5
+        segdata$exposure[segdata$b3 >= (segdata$tl - ageseg[[i]][1]) &
+                           segdata$b3 < (segdata$tu - ageseg[[i]][2]) ] <- 1
 
-    segdata$death[segdata$b3 >= (segdata$tl - ageseg[[i]][2]) &
-                    segdata$b3 < (segdata$tl - ageseg[[i]][1]) &
-                    (segdata$b7 >= ageseg[[i]][1] & segdata$b7 < ageseg[[i]][2])] <- 0.5
+        segdata$exposure[segdata$b3 >= (segdata$tu - ageseg[[i]][2]) &
+                           segdata$b3 < (segdata$tu - ageseg[[i]][1]) ] <- 0.5
 
-    segdata$death[segdata$b3 >= (segdata$tl - ageseg[[i]][1]) &
-                    segdata$b3 < (segdata$tu - ageseg[[i]][2]) &
-                    (segdata$b7 >= ageseg[[i]][1] & segdata$b7 < ageseg[[i]][2])] <- 1
+        segdata$death <- NA
 
-    segdata$death[segdata$b3 >= (segdata$tu - ageseg[[i]][2]) &
-                    segdata$b3 < (segdata$tu - ageseg[[i]][1]) &
-                    (segdata$b7 >= ageseg[[i]][1] & segdata$b7 < ageseg[[i]][2])] <- ifelse(is.null(PeriodEnd) , 1, 0.5)
+        segdata$death[segdata$b3 >= (segdata$tl - ageseg[[i]][2]) &
+                        segdata$b3 < (segdata$tl - ageseg[[i]][1]) &
+                        (segdata$b7 >= ageseg[[i]][1] & segdata$b7 < ageseg[[i]][2])] <- 0.5
 
-    segdata$death[is.na(segdata$b7)] <- 0
+        segdata$death[segdata$b3 >= (segdata$tl - ageseg[[i]][1]) &
+                        segdata$b3 < (segdata$tu - ageseg[[i]][2]) &
+                        (segdata$b7 >= ageseg[[i]][1] & segdata$b7 < ageseg[[i]][2])] <- 1
 
-    deathprob[names(ageseg)[i]] <- sum(segdata$death * segdata$rweight, na.rm = TRUE) / (sum(segdata$exposure * segdata$rweight, na.rm = TRUE))
-  }
+        segdata$death[segdata$b3 >= (segdata$tu - ageseg[[i]][2]) &
+                        segdata$b3 < (segdata$tu - ageseg[[i]][1]) &
+                        (segdata$b7 >= ageseg[[i]][1] & segdata$b7 < ageseg[[i]][2])] <- ifelse(is.null(PeriodEnd) , 1, 0.5)
 
-  mort_rates <- list(
-    c(data.frame(dpro = deathprob[1:1])),
-    c(data.frame(dpro = deathprob[1:4])),
-    c(data.frame(dpro = deathprob[5:8])),
-    c(data.frame(dpro = deathprob[1:8]))
-  )
-  names(mort_rates) <- c("NNMR", "IMR", "CMR", "U5MR")
+        segdata$death[is.na(segdata$b7)] <- 0
 
-  mort_res <- numeric(length = 5)
-  names(mort_res) <- c("NNMR", "PNNMR", "IMR", "CMR", "U5MR")
+        deathprob[names(ageseg)[i]] <- sum(segdata$death * segdata$rweight, na.rm = TRUE) / (sum(segdata$exposure * segdata$rweight, na.rm = TRUE))
+      }
 
-  for (i in seq_along(mort_rates)) {
-    mort <- mort_rates[[i]]
-    mort_label <- names(mort_rates)[i]
-    mort$dpro <- (1 - mort$dpro)
+      mort_rates <- list(
+        c(data.frame(dpro = deathprob[1:1])),
+        c(data.frame(dpro = deathprob[1:4])),
+        c(data.frame(dpro = deathprob[5:8])),
+        c(data.frame(dpro = deathprob[1:8]))
+      )
+      names(mort_rates) <- c("NNMR", "IMR", "CMR", "U5MR")
 
-    dpromat <- data.matrix(mort$dpro)
-    product <- matrixStats::colProds(dpromat)
+      mort_res <- numeric(length = 5)
+      names(mort_res) <- c("NNMR", "PNNMR", "IMR", "CMR", "U5MR")
 
-    mort_res[mort_label] <- (abs(1 - product) * 1000)
-  }
-  mort_res[2] <- mort_res[3] - mort_res[1]
+      for (i in seq_along(mort_rates)) {
+        mort <- mort_rates[[i]]
+        mort_label <- names(mort_rates)[i]
+        mort$dpro <- (1 - mort$dpro)
 
-  list(mort_res)
-}
+        dpromat <- data.matrix(mort$dpro)
+        product <- matrixStats::colProds(dpromat)
+
+        mort_res[mort_label] <- (abs(1 - product) * 1000)
+      }
+      mort_res[2] <- mort_res[3] - mort_res[1]
+
+      list(mort_res)
+    }
+
 
 ### N: Total births contributed to the calculations
 NBIRTHS <- function(Data.Name) {
-  BirthNg <- list(c(0, 1), c(1, 12), c(0, 12), c(12, 60), c(0, 60))
-  names(BirthNg) <- c("NNMR", "PNNMR", "IMR", "CMR", "U5MR")
+      BirthNg <- list(c(0, 1), c(1, 12), c(0, 12), c(12, 60), c(0, 60))
+      names(BirthNg) <- c("NNMR", "PNNMR", "IMR", "CMR", "U5MR")
 
-  BirthN <- numeric(length = 5)
-  names(BirthN) <- names(BirthNg)
+      BirthN <- numeric(length = 5)
+      names(BirthN) <- names(BirthNg)
 
-  for (i in seq_along(BirthNg)) {
-    segdata <- Data.Name[which((Data.Name$b7 >= BirthNg[[i]][1]) | is.na(Data.Name$b7)), ]
+      for (i in seq_along(BirthNg)) {
+        segdata <- Data.Name[which((Data.Name$b7 >= BirthNg[[i]][1]) | is.na(Data.Name$b7)), ]
 
-    segdata$exposure[segdata$b3 >= (segdata$tl - BirthNg[[i]][2])
-                     & segdata$b3 < (segdata$tu - BirthNg[[i]][1]) ] <- 1
+        segdata$exposure <- NA
 
-    BirthN[names(BirthNg)[i]] <- sum(segdata$exposure, na.rm = TRUE)
-  }
+        segdata$exposure[segdata$b3 >= (segdata$tl - BirthNg[[i]][2]) &
+                           segdata$b3 < (segdata$tl - BirthNg[[i]][1]) ] <- 0.5
 
-  list(BirthN)
-}
+        segdata$exposure[segdata$b3 >= (segdata$tl - BirthNg[[i]][1]) &
+                           segdata$b3 < (segdata$tu - BirthNg[[i]][2]) ] <- 1
+
+        segdata$exposure[segdata$b3 >= (segdata$tu - BirthNg[[i]][2]) &
+                           segdata$b3 < (segdata$tu - BirthNg[[i]][1]) ] <- 0.5
+
+        BirthN[names(BirthNg)[i]] <- sum(segdata$exposure, na.rm = TRUE)
+
+      }
+
+      list(BirthN)
+    }
+
+
+### WN: Total births contributed to the calculations _ weighted
+NBIRTHSW <- function(Data.Name) {
+      BirthNg <- list(c(0, 1), c(1, 12), c(0, 12), c(12, 60), c(0, 60))
+      names(BirthNg) <- c("NNMR", "PNNMR", "IMR", "CMR", "U5MR")
+
+      BirthNW <- numeric(length = 5)
+      names(BirthNW) <- names(BirthNg)
+
+      for (i in seq_along(BirthNg)) {
+        segdata <- Data.Name[which((Data.Name$b7 >= BirthNg[[i]][1]) | is.na(Data.Name$b7)), ]
+
+        segdata$exposure <- NA
+
+        segdata$exposure[segdata$b3 >= (segdata$tl - BirthNg[[i]][2]) &
+                           segdata$b3 < (segdata$tl - BirthNg[[i]][1]) ] <- 0.5
+
+        segdata$exposure[segdata$b3 >= (segdata$tl - BirthNg[[i]][1]) &
+                           segdata$b3 < (segdata$tu - BirthNg[[i]][2]) ] <- 1
+
+        segdata$exposure[segdata$b3 >= (segdata$tu - BirthNg[[i]][2]) &
+                           segdata$b3 < (segdata$tu - BirthNg[[i]][1]) ] <- 0.5
+
+        BirthNW[names(BirthNg)[i]] <- sum(segdata$exposure * segdata$rweight, na.rm = TRUE)
+      }
+
+      list(BirthNW)
+    }
+
+
+### DEFT: design effect
+DEFT <- function(Data.Name) {
+      BirthNg <- list(c(0, 1), c(1, 12), c(0, 12), c(12, 60), c(0, 60))
+      names(BirthNg) <- c("NNMR", "PNNMR", "IMR", "CMR", "U5MR")
+
+      Deft <- numeric(length = 5)
+      names(Deft) <- names(BirthNg)
+
+      for (i in seq_along(BirthNg)) {
+        segdata <- Data.Name[which((Data.Name$b7 >= BirthNg[[i]][1]) | is.na(Data.Name$b7)), ]
+
+        segdata$exposure <- 0
+
+        segdata$exposure[segdata$b3 >= (segdata$tl - BirthNg[[i]][2]) &
+                           segdata$b3 < (segdata$tl - BirthNg[[i]][1]) ] <- 0.5
+
+        segdata$exposure[segdata$b3 >= (segdata$tl - BirthNg[[i]][1]) &
+                           segdata$b3 < (segdata$tu - BirthNg[[i]][2]) ] <- 1
+
+        segdata$exposure[segdata$b3 >= (segdata$tu - BirthNg[[i]][2]) &
+                           segdata$b3 < (segdata$tu - BirthNg[[i]][1]) ] <- 0.5
+
+        segdata$death <- 0
+
+        segdata$death[segdata$b3 >= (segdata$tl - BirthNg[[i]][2]) &
+                        segdata$b3 < (segdata$tl - BirthNg[[i]][1]) &
+                        (segdata$b7 >= BirthNg[[i]][1] & segdata$b7 < BirthNg[[i]][2])] <- 0.5
+
+        segdata$death[segdata$b3 >= (segdata$tl - BirthNg[[i]][1]) &
+                        segdata$b3 < (segdata$tu - BirthNg[[i]][2]) &
+                        (segdata$b7 >= BirthNg[[i]][1] & segdata$b7 < BirthNg[[i]][2])] <- 1
+
+        segdata$death[segdata$b3 >= (segdata$tu - BirthNg[[i]][2]) &
+                        segdata$b3 < (segdata$tu - BirthNg[[i]][1]) &
+                        (segdata$b7 >= BirthNg[[i]][1] & segdata$b7 < BirthNg[[i]][2])] <- 0.5
+
+        segdata$death[is.na(segdata$b7)] <- 0
+
+        dstrat<-survey::svydesign(id = ~v021, strata = ~v022, weights = ~rweight, data = segdata)
+
+        Deft[names(BirthNg)[i]] <- sqrt(survey::deff(survey::svyratio(~death, ~exposure, dstrat, deff = "replace")))
+      }
+
+      list(Deft)
+    }
--- R/chmort.R
+++ R/chmort.R
@@ -3,8 +3,8 @@
 #' \code{chmort} returns childhood mortality rates such as Neonatal Mortality Rate (NNMR),
 #' Post-neonatal Mortality Rate (PNNMR), Infant Mortality Rate (IMR), Child Mortality Rate (CMR),
 #' and Under-5 Mortality Rate (U5MR)
-#' \code{chmort} returns the Standrad Error (SE), Number of births contributed to the death exposure (N),
-#' the Relative Standard Error (RSE) and the Confidence Interval (CI).
+#' \code{chmort} returns the Standrad Error (SE), the mortality exposure (N), the weighted exposure (WN),
+#' the Design Effect (DEFT), the Relative Standard Error (RSE) and the Confidence Interval (CI).
 #'
 #' @author Mahmoud Elkasabi.
 #'
@@ -64,162 +64,169 @@
 #'
 #' @export
 chmort <- function(Data.Name, JK = NULL, Strata = NULL, Cluster = NULL, Weight = NULL, Year_of_survey = NULL,
-                   Date_of_interview = NULL, Date_of_birth = NULL, Date_of_death = NULL, PeriodEnd = NULL,
-                   Period = NULL, Class = NULL){
+                       Date_of_interview = NULL, Date_of_birth = NULL, Date_of_death = NULL, PeriodEnd = NULL,
+                       Period = NULL, Class = NULL){
 
-    if (!is.null(Strata)){
-    Data.Name$strata = Data.Name[[Strata]]
-    Data.Name$v022 = NULL
-    names(Data.Name)[names(Data.Name) == c("strata")] <- c("v022")
-  }
-
-  if (!is.null(Cluster)){
-    Data.Name$cluster = Data.Name[[Cluster]]
-    Data.Name$v021 = NULL
-    names(Data.Name)[names(Data.Name) == c("cluster")] <- c("v021")
-  }
-
-  if (!is.null(Weight)){
-    Data.Name$weight = Data.Name[[Weight]]
-    Data.Name$v005 = NULL
-    names(Data.Name)[names(Data.Name) == c("weight")] <- c("v005")
-  }
-
-  if (!is.null(Year_of_survey)){
-    Data.Name$YOS = Data.Name[[Year_of_survey]]
-    Data.Name$v007 = NULL
-    names(Data.Name)[names(Data.Name) == c("YOS")] <- c("v007")
-  }
-
-  if (!is.null(Date_of_interview)){
-    Data.Name$DOI = Data.Name[[Date_of_interview]]
-    Data.Name$v008 = NULL
-    names(Data.Name)[names(Data.Name) == c("DOI")] <- c("v008")
-  }
-
-  if (!is.null(Date_of_birth)){
-    Data.Name$BDOB = Data.Name[[Date_of_birth]]
-    Data.Name$b3 = NULL
-    names(Data.Name)[names(Data.Name) == c("BDOB")] <- c("b3")
-  }
-
-  if (!is.null(Date_of_death)){
-    Data.Name$BDOD = Data.Name[[Date_of_death]]
-    Data.Name$b7 = NULL
-    names(Data.Name)[names(Data.Name) == c("BDOD")] <- c("b7")
-  }
-
-  if (!("v021" %in% names(Data.Name))) stop({message("Error: v021/Primary-sampling-unit is missing")})
-  if (!("v005" %in% names(Data.Name))) stop({message("Error: v005/Sample-weight is missing")})
-  if (!("v007" %in% names(Data.Name))) stop({message("Error: v007/Year-of-survey is missing")})
-  if (!("v008" %in% names(Data.Name))) stop({message("Error: v008/Date-of-Interview is missing")})
-  if (!("v022" %in% names(Data.Name))) stop({message("Error: v022/Sample-strata is missing")})
-  if (!("b3" %in% names(Data.Name))) stop({message("Error: b3/Date-of-birth is missing")})
-  if (!("b7" %in% names(Data.Name))) stop({message("Error: b7/Date-of-death is missing")})
-
- Data.ready <- as.data.frame(Data.Name[!Data.Name$v005 == 0,])
-
-  if (is.null(Period)){Data.ready$period = 60}
-   else {Data.ready$period = Period * 12}   # refence period in years
-
-  if (is.null(PeriodEnd)){Data.ready$periodend = 0}
-  else {Data.ready$periodend = (Data.ready$v007 - PeriodEnd) * 12}  # End of refence period in years;
-  #0 when the end is the DOI
-
-  Data.ready$rweight = Data.ready$v005/1000000
-
-  #Time period
-  Data.ready$tu <- (Data.ready$v008 - Data.ready$periodend)
-  Data.ready$tl <- (Data.ready$v008 - Data.ready$periodend) - Data.ready$period
-
-  #######For Overall Indicators; no Class ########################################
-  if (is.null(Class)){
-
-  chmortdat<- Data.ready[, c("v021", "v022", "rweight", "v007", "v008", "b3", "b7", "tu", "tl","periodend")]
-  chmortdat$id <- c(as.factor(chmortdat$v021))
-
-    if (is.null(JK)){PSU <- 0} else {PSU <- max(chmortdat$id)}
-    CHMORT = CHMORT5(chmortdat,PeriodEnd = PeriodEnd)[[1]]
-    NBIRTH = NBIRTHS(chmortdat)[[1]]
-
-    JKres <- matrix(0, nrow = PSU, ncol = 5)
-    dimnames(JKres) <- list(NULL, c("NNMR", "PNNMR", "IMR", "CMR", "U5MR"))
-
-    if (is.null(JK)){
-
-      RESULTcmr <- matrix(0, nrow = 5, ncol = 2)
-      dimnames(RESULTcmr) <- list(c("NNMR", "PNNMR", "IMR", "CMR", "U5MR" ),c("Rate","N"))
-      for (i in 1:(nrow(RESULTcmr))) {
-        RESULTcmr[i,1] = CHMORT[i]
-        RESULTcmr[i,2] = NBIRTH[i]
-        }
-      list(round(RESULTcmr,2))
-
-    } else {
-
-      for (i in unique(chmortdat$id))
-        {
-        chmortdatJ <- chmortdat[which(!chmortdat$id == i),]
-        JKres[i,] <- CHMORT5(chmortdatJ,PeriodEnd = PeriodEnd)[[1]]
-        }
-
-      RESULTcmr <- matrix(0, nrow = 5, ncol = 7)
-      dimnames(RESULTcmr) <- list(c("NNMR", "PNNMR", "IMR", "CMR", "U5MR"),
-                                  c("Rate", "SE", "N", "RSE", "LCI", "UCI", "iterations"))
-      for (i in 1:(nrow(RESULTcmr))) {
-        RESULTcmr[i, 1] = CHMORT[i]
-        RESULTcmr[i, 2] = sqrt(sum(((PSU * CHMORT[i] - (PSU-1) * JKres[, i])-CHMORT[i])^2)/(PSU * (PSU-1)))
-        RESULTcmr[i, 3] = NBIRTH[i]
-        RESULTcmr[i, 4] = RESULTcmr[i, 2] / RESULTcmr[i, 1]
-        RESULTcmr[i, 5] = RESULTcmr[i, 1] - (2 * RESULTcmr[i, 2])
-        RESULTcmr[i, 6] = RESULTcmr[i, 1] + (2 * RESULTcmr[i, 2])
-        RESULTcmr[i, 7] = PSU
-        }
-      list(round(RESULTcmr, 2))
+      if (!is.null(Strata)){
+        Data.Name$strata = Data.Name[[Strata]]
+        Data.Name$v022 = NULL
+        names(Data.Name)[names(Data.Name) == c("strata")] <- c("v022")
       }
-  }
 
-  #######For Class Indicators; #################################################################################################
-  else{
+      if (!is.null(Cluster)){
+        Data.Name$cluster = Data.Name[[Cluster]]
+        Data.Name$v021 = NULL
+        names(Data.Name)[names(Data.Name) == c("cluster")] <- c("v021")
+      }
 
-    chmortdat<- Data.ready[, c("v021", "v022", "rweight", "v007", "v008", "b3", "b7", "tu", "tl",
-                               "periodend", Class)]
+      if (!is.null(Weight)){
+        Data.Name$weight = Data.Name[[Weight]]
+        Data.Name$v005 = NULL
+        names(Data.Name)[names(Data.Name) == c("weight")] <- c("v005")
+      }
 
-    chmortdat$DomID  <- c(as.factor(chmortdat[[Class]]))
+      if (!is.null(Year_of_survey)){
+        Data.Name$YOS = Data.Name[[Year_of_survey]]
+        Data.Name$v007 = NULL
+        names(Data.Name)[names(Data.Name) == c("YOS")] <- c("v007")
+      }
 
-    RESULTS <- matrix(0, 0, ncol = 3)
-    dimnames(RESULTS) <- list(NULL, c("Class", "R", "N") )
-    RESULTS <- as.data.frame(RESULTS)
+      if (!is.null(Date_of_interview)){
+        Data.Name$DOI = Data.Name[[Date_of_interview]]
+        Data.Name$v008 = NULL
+        names(Data.Name)[names(Data.Name) == c("DOI")] <- c("v008")
+      }
 
-    RESULTSJK <- matrix(0, 0, ncol = 8)
-    dimnames(RESULTSJK) <- list(NULL, c("Class", "R", "SE", "N", "RSE", "LCI", "UCI", "iterations"))
-    RESULTSJK <- as.data.frame(RESULTSJK)
+      if (!is.null(Date_of_birth)){
+        Data.Name$BDOB = Data.Name[[Date_of_birth]]
+        Data.Name$b3 = NULL
+        names(Data.Name)[names(Data.Name) == c("BDOB")] <- c("b3")
+      }
 
-    for (j in 1:(max(chmortdat$DomID))) {
+      if (!is.null(Date_of_death)){
+        Data.Name$BDOD = Data.Name[[Date_of_death]]
+        Data.Name$b7 = NULL
+        names(Data.Name)[names(Data.Name) == c("BDOD")] <- c("b7")
+      }
 
-      DatD = as.data.frame(chmortdat[chmortdat$DomID == j, ])
-      DatD$id <- NULL
-      DatD$id <- c(as.factor(DatD$v021))
-      DatD[[Class]] = haven::as_factor(DatD[[Class]])
+      if (!("v021" %in% names(Data.Name))) stop({message("Error: v021/Primary-sampling-unit is missing")})
+      if (!("v005" %in% names(Data.Name))) stop({message("Error: v005/Sample-weight is missing")})
+      if (!("v007" %in% names(Data.Name))) stop({message("Error: v007/Year-of-survey is missing")})
+      if (!("v008" %in% names(Data.Name))) stop({message("Error: v008/Date-of-Interview is missing")})
+      if (!("v022" %in% names(Data.Name))) stop({message("Error: v022/Sample-strata is missing")})
+      if (!("b3" %in% names(Data.Name))) stop({message("Error: b3/Date-of-birth is missing")})
+      if (!("b7" %in% names(Data.Name))) stop({message("Error: b7/Date-of-death is missing")})
 
-      CHMORT = CHMORT5(DatD, PeriodEnd = PeriodEnd)[[1]]
-      NBIRTH = NBIRTHS(DatD)[[1]]
+      Data.ready <- as.data.frame(Data.Name[!Data.Name$v005 == 0,])
+
+      if (is.null(Period)){Data.ready$period = 60}
+      else {Data.ready$period = Period * 12}   # refence period in years
+
+      if (is.null(PeriodEnd)){Data.ready$periodend = 0}
+      else {Data.ready$periodend = (Data.ready$v007 - PeriodEnd) * 12}  # End of refence period in years;
+      #0 when the end is the DOI
+
+      Data.ready$rweight = Data.ready$v005/1000000
+
+      #Time period
+      Data.ready$tu <- (Data.ready$v008 - Data.ready$periodend)
+      Data.ready$tl <- (Data.ready$v008 - Data.ready$periodend) - Data.ready$period
+
+      #######For Overall Indicators; no Class ########################################
+      if (is.null(Class)){
+
+        chmortdat<- Data.ready[, c("v021", "v022", "rweight", "v007", "v008", "b3", "b7", "tu", "tl","periodend")]
+        chmortdat$id <- c(as.factor(chmortdat$v021))
+
+        if (is.null(JK)){PSU <- 0} else {PSU <- max(chmortdat$id)}
+        CHMORT = CHMORT5(chmortdat,PeriodEnd = PeriodEnd)[[1]]
+        NBIRTH = NBIRTHS(chmortdat)[[1]]
+        NBIRTHW= NBIRTHSW(chmortdat)[[1]]
+        DEFF = DEFT(chmortdat)[[1]]
+
+        JKres <- matrix(0, nrow = PSU, ncol = 5)
+        dimnames(JKres) <- list(NULL, c("NNMR", "PNNMR", "IMR", "CMR", "U5MR"))
 
         if (is.null(JK)){
 
-            RESULTS0 <- cbind.data.frame(attributes(DatD[[Class]])$levels[[j]],
-                                         round(CHMORT, 2), NBIRTH)
+          RESULTcmr <- matrix(0, nrow = 5, ncol = 3)
+          dimnames(RESULTcmr) <- list(c("NNMR", "PNNMR", "IMR", "CMR", "U5MR" ),c("Rate","N","WN"))
+          for (i in 1:(nrow(RESULTcmr))) {
+            RESULTcmr[i,1] = round(CHMORT[i],2)
+            RESULTcmr[i,2] = round(NBIRTH[i],0)
+            RESULTcmr[i,3] = round(NBIRTHW[i],0)
+          }
+          list(RESULTcmr)
 
-            names(RESULTS0) <- c("Class", "R", "N")
+        } else {
+
+          for (i in unique(chmortdat$id))
+          {
+            chmortdatJ <- chmortdat[which(!chmortdat$id == i),]
+            JKres[i,] <- CHMORT5(chmortdatJ,PeriodEnd = PeriodEnd)[[1]]
+          }
+
+          RESULTcmr <- matrix(0, nrow = 5, ncol = 9)
+          dimnames(RESULTcmr) <- list(c("NNMR", "PNNMR", "IMR", "CMR", "U5MR"),
+                                      c("Rate", "SE", "N", "WN", "DEFT", "RSE", "LCI", "UCI", "iterations"))
+          for (i in 1:(nrow(RESULTcmr))) {
+            RESULTcmr[i, 1] = round(CHMORT[i],2)
+            RESULTcmr[i, 2] = round(sqrt(sum(((PSU * CHMORT[i] - (PSU-1) * JKres[, i])-CHMORT[i])^2)/(PSU * (PSU-1))),2)
+            RESULTcmr[i, 3] = round(NBIRTH[i],0)
+            RESULTcmr[i, 4] = round(NBIRTHW[i],0)
+            RESULTcmr[i, 5] = round(DEFF[i],2)
+            RESULTcmr[i, 6] = round(RESULTcmr[i, 2] / RESULTcmr[i, 1],2)
+            RESULTcmr[i, 7] = round(RESULTcmr[i, 1] - (2 * RESULTcmr[i, 2]),2)
+            RESULTcmr[i, 8] = round(RESULTcmr[i, 1] + (2 * RESULTcmr[i, 2]),2)
+            RESULTcmr[i, 9] = PSU
+          }
+          list(RESULTcmr)
+        }
+      }
+
+      #######For Class Indicators; #################################################################################################
+      else{
+
+        chmortdat<- Data.ready[, c("v021", "v022", "rweight", "v007", "v008", "b3", "b7", "tu", "tl",
+                                   "periodend", Class)]
+
+        chmortdat$DomID  <- c(as.factor(chmortdat[[Class]]))
+
+        RESULTS <- matrix(0, 0, ncol = 4)
+        dimnames(RESULTS) <- list(NULL, c("Class", "R", "N", "WN") )
+        RESULTS <- as.data.frame(RESULTS)
+
+        RESULTSJK <- matrix(0, 0, ncol = 10)
+        dimnames(RESULTSJK) <- list(NULL, c("Class", "R", "SE", "N", "WN", "DEFT", "RSE", "LCI", "UCI", "iterations"))
+        RESULTSJK <- as.data.frame(RESULTSJK)
+
+        for (j in 1:(max(chmortdat$DomID))) {
+
+          DatD = as.data.frame(chmortdat[chmortdat$DomID == j, ])
+          DatD$id <- NULL
+          DatD$id <- c(as.factor(DatD$v021))
+          DatD[[Class]] = haven::as_factor(DatD[[Class]])
+
+          CHMORT = CHMORT5(DatD, PeriodEnd = PeriodEnd)[[1]]
+          NBIRTH = NBIRTHS(DatD)[[1]]
+          NBIRTHW = NBIRTHSW(DatD)[[1]]
+          DEFF = DEFT(DatD)[[1]]
+
+          if (is.null(JK)){
+
+            RESULTS0 <- cbind.data.frame(attributes(DatD[[Class]])$levels[[j]],
+                                         round(CHMORT, 2), round(NBIRTH,0), round(NBIRTHW,0))
+
+            names(RESULTS0) <- c("Class", "R", "N", "WN")
             RESULTS = rbind(RESULTS, RESULTS0)
 
-        }else {
+          }else {
 
-          if (is.null(JK)){PSU <- 0} else {PSU <- max(DatD$id)}
-          if  (PSU == 1)  {warning("Error: a single cluster Class")}
+            if (is.null(JK)){PSU <- 0} else {PSU <- max(DatD$id)}
+            if  (PSU == 1)  {warning("Error: a single cluster Class")}
 
-          JKres <- matrix(0, nrow = 5, ncol = 0)
-          dimnames(JKres) <- list(c("NNMR", "PNNMR", "IMR", "CMR", "U5MR"), NULL)
+            JKres <- matrix(0, nrow = 5, ncol = 0)
+            dimnames(JKres) <- list(c("NNMR", "PNNMR", "IMR", "CMR", "U5MR"), NULL)
 
             for (i in unique(DatD$id))
             {
@@ -229,23 +236,23 @@ chmort <- function(Data.Name, JK = NULL, Strata = NULL, Cluster = NULL, Weight =
               JKres = cbind(JKres, JKres0)
             }
 
-          RateSE <- numeric(length = 5)
-          names(RateSE) <- c("NNMR", "PNNMR", "IMR", "CMR", "U5MR")
+            RateSE <- numeric(length = 5)
+            names(RateSE) <- c("NNMR", "PNNMR", "IMR", "CMR", "U5MR")
 
             for (i in seq_along (RateSE)) {
-            JKSE = ((PSU * CHMORT[[i]] - (PSU-1) * JKres[i, ])-CHMORT[[i]])^2
-            RateSE[names(RateSE)[i]] = sqrt(sum(JKSE)/(PSU * (PSU-1)))
+              JKSE = ((PSU * CHMORT[[i]] - (PSU-1) * JKres[i, ])-CHMORT[[i]])^2
+              RateSE[names(RateSE)[i]] = sqrt(sum(JKSE)/(PSU * (PSU-1)))
             }
 
-          RESULTS0 <- cbind.data.frame(attributes(DatD[[Class]])$levels[[j]], round(CHMORT, 2),
-                                       round(RateSE, 2), NBIRTH, round(RateSE/CHMORT, 2),
-                                       round(CHMORT - 2 * RateSE, 2), round(CHMORT + 2 * RateSE, 2),
-                                       PSU)
+            RESULTS0 <- cbind.data.frame(attributes(DatD[[Class]])$levels[[j]], round(CHMORT, 2),
+                                         round(RateSE, 2), round(NBIRTH,0), round(NBIRTHW,0), round(DEFF,2), round(RateSE/CHMORT, 2),
+                                         round(CHMORT - 2 * RateSE, 2), round(CHMORT + 2 * RateSE, 2),
+                                         PSU)
 
-          names(RESULTS0) <- c("Class", "R", "SE", "N", "RSE", "LCI", "UCI", "iterations")
-          RESULTSJK = rbind(RESULTSJK, RESULTS0)
+            names(RESULTS0) <- c("Class", "R", "SE", "N", "WN", "DEFT", "RSE", "LCI", "UCI", "iterations")
+            RESULTSJK = rbind(RESULTSJK, RESULTS0)
+          }
+        }
+        if (is.null(JK)){list(RESULTS)} else {list(RESULTSJK)}
       }
     }
-    if (is.null(JK)){list(RESULTS)} else {list(RESULTSJK)}
-  }
-}
